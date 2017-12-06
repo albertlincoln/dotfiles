1c60be2faf646fafb89d13a76600c1d0bb45d446
Albert Lash <albert.lash@gmail.com>
2017-12-06 23:34:47 +0000

    better file mgmt

--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -5,9 +5,11 @@ set -e
 set -x
 
 cd /home/$(whoami)
-for file in .bashrc .profile .vimrc .gitconfig; do
+for file in .bashrc .profile .vimrc .gitconfig .ssh/config; do
     echo $file
+    mkdir -p $(basename /home/$(whoami)/$file)
     rm -f $file
     curl -O https://raw.githubusercontent.com/albertlincoln/dotfiles/master/home/${file}
-
 done
+
+
