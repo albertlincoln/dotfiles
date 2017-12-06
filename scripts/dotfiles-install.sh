97cd800d6efdd42932214e97bb110699978d4a89
Albert Lash <albert.lash@gmail.com>
2017-12-06 23:36:54 +0000

    better file mgmt, and backups

--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -7,8 +7,11 @@ set -x
 cd /home/$(whoami)
 for file in .bashrc .profile .vimrc .gitconfig .ssh/config; do
     echo $file
-    mkdir -p $(basename /home/$(whoami)/$file)
-    rm -f $file
+    BACKUPDIR=.dotfile_backups/$(date +%s)
+    mkdir -p ${BACKUPDIR}
+    mkdir -p $(dirname /home/$(whoami)/$file)
+    touch $file
+    mv $file $BACKUPDIR/
     curl -O https://raw.githubusercontent.com/albertlincoln/dotfiles/master/home/${file}
 done
 
