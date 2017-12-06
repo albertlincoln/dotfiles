f0fffc75c0de28b3812bebc8a26362bb6f2b383c
Albert Lash <31164347+albertlincoln@users.noreply.github.com>
2017-12-06 22:48:34 +0000

    Update dotfiles-install.sh

--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -1,5 +1,5 @@
-## Download all the files - run like curl "https://raw.githubusercontent.com/albertlincoln/dotfiles/master/scripts/dotfiles-install.sh?token=AduHu6RqxNSI6I4_MoKmfgEks5aMazBwA%3D%3D"
+## Download all the files - run like curl "https://raw.githubusercontent.com/albertlincoln/dotfiles/master/scripts/dotfiles-install.sh?token=${TOKEN}"
 
 cd /home/${whoami}
 
-curl -O https://github.com/albertlincoln/dotfiles/raw/master/home/.gitconfig
+curl -O https://github.com/albertlincoln/dotfiles/raw/master/home/.gitconfig?token=${TOKEN}
