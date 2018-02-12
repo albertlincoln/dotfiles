cc943a6ea4e1bac466e088bd958adce5c307b1dc
Albert Lash <albert.lash@gmail.com>
2018-02-12 18:29:29 -0500

    Added some packages, fixed a bug

---
+++ scripts/debian.sh
@@ -0,0 +1,7 @@
+sudo apt-get install \
+    vim-addon-manager \
+    vim-nox \
+    vim-scripts \
+    bash-completion \
+    pass \
+    iputils-ping \
--- scripts/dotfiles-install.sh
+++ scripts/dotfiles-install.sh
@@ -1,25 +1,26 @@
 # Download all the files - run like 
-# url "https://raw.githubusercontent.com/albertlincoln/dotfiles/master/scripts/dotfiles-install.sh" | bash
+# curl "https://raw.githubusercontent.com/albertlincoln/dotfiles/master/scripts/dotfiles-install.sh" | bash
 HOME=/home/$(whoami)
 BACKUPS=${HOME}/.dotfile_backups
 BACKUPDIR=${BACKUPS}/$(date +%s)
+MY_REPO_DIR="https://raw.githubusercontent.com/albertlincoln/dotfiles/master"
 mkdir -p ${BACKUPDIR}
 
 set -ex
-if [ -x $(which fdupes) ]; then
+if [ "$(which fdupes)" == "0" ]; then
 	fdupes -r -N -d ${BACKUPS}  > ${BACKUPDIR}/cleanup.log 2>&1
 	find ${BACKUPS} -type d -empty -delete
 fi
 
 set +x
-#for file in .bashrc .profile .vimrc .gitconfig .ssh/config ; do
-for file in $(curl https://raw.githubusercontent.com/albertlincoln/dotfiles/master/manifests/home.txt | xargs); do
+
+for file in $(curl ${MY_REPO_DIR}/manifests/home.txt | xargs); do
     cd ${HOME}
     echo $file
     mkdir -p ${BACKUPDIR} $(dirname ${HOME}/$file)
     touch $file
     mv $file $BACKUPDIR/
-    curl -s -o $file https://raw.githubusercontent.com/albertlincoln/dotfiles/master/home/${file}
+    curl -s -o $file $MY_REPO_DIR/home/${file}
 done
 set -x
 
