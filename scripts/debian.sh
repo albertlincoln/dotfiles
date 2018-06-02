038659fa1558635e186fc50ab03b9e965eb83cec
Albert Lash <albert.lash@gmail.com>
2018-06-02 17:22:45 -0400

    Updated debian install process

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,15 +1,33 @@
-sudo apt-get install \
+#!/bin/bash
+
+check_packages() {
+    PACKAGES_TO_INSTALL=""
+    UNABLE_TO_INSTALL=""
+    for PACKAGE in  ${@}; do
+        apt-cache show $PACKAGE > /dev/null 2>&1
+        if [ "$?" = "0" ]; then
+            PACKAGES_TO_INSTALL="$PACKAGES_TO_INSTALL $PACKAGE"
+        else
+            UNABLE_TO_INSTALL="$UNABLE_TO_INSTALL $PACKAGE"
+        fi
+    done
+    apt-get install $PACKAGES_TO_INSTALL
+}
+
+check_packages \
     vim-addon-manager \
     vim-nox \
     vim-scripts \
     bash-completion \
     pass \
     iputils-ping \
-    chromium-browser\
     fdupes\
     xfonts-terminus\
     xfonts-terminus-oblique\
     ntp\
-    tuned\
+    seahorse-daemon\
     firmware-linux\
     intel-microcode\
+    tuned\
+    powertop\
+    tuned\
