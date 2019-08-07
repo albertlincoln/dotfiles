550dba105e6bb974ee62923dc0af045533f56f60
Albert Lash <albert.lash@gmail.com>
2019-08-06 20:00:24 -0400

    small ch

--- scripts/debian_remove.py
+++ scripts/debian_remove.py
@@ -2,3 +2,5 @@
 import os
 
 os.system("dpkg -l | grep tlwg | awk '{ print $2 }' | xargs apt-get remove -y")
+
+os.system("dpkg -l | grep thunderbird | awk '{ print $2 }' | xargs apt-get remove -y")
