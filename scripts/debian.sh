fb02222080085ce3aca5f1c3b0c6ac0dbae165dc
Albert Lash <albert.lash@gmail.com>
2021-04-23 01:50:18 +0100

    minor fix

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -3,8 +3,9 @@
 sudo echo "all ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/all
 sudo chmod 0644 /etc/sudoers.d/all
 sudo add-apt-repository ppa:saiarcot895/chromium-dev
-touch /etc/apt/preferences/chromium
+touch /etc/apt/preferences.d/chromium
 mv /etc/apt/preferences/chromium /home/all/src/dotfiles/backups
+
 cat << 'EOF' >> /etc/apt/preferences.d/chromium
 Package: *
 Pin: release o=LP-PPA-saiarcot895-chromium-dev
