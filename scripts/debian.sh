43f3178bd8369c8a9b01ebdf1623ba3313c4d8cf
Albert Lash <albert.lash@gmail.com>
2021-04-29 12:40:29 +0100

    making debian.sh script more generic

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -2,14 +2,19 @@
 
 sudo echo "all ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/all
 sudo chmod 0644 /etc/sudoers.d/all
-sudo add-apt-repository ppa:saiarcot895/chromium-dev
-touch /etc/apt/preferences.d/chromium
-mv /etc/apt/preferences/chromium /home/all/src/dotfiles/backups
 
-cat << 'EOF' >> /etc/apt/preferences.d/chromium
+
+if [ -x /usr/bin/apt ]; then
+    sudo add-apt-repository ppa:saiarcot895/chromium-dev
+
+    touch /etc/apt/preferences.d/chromium
+    mv /etc/apt/preferences.d/chromium /home/all/src/dotfiles/backups
+
+    cat << 'EOF' >> /etc/apt/preferences.d/chromium
 Package: *
 Pin: release o=LP-PPA-saiarcot895-chromium-dev
 Pin-Priority: 1002
 EOF
 
-sudo apt install chromium-browser -y
+    sudo apt install chromium-browser -y
+fi
